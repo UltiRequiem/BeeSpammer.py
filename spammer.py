365898e6370419db24c2b4d5676f7557bf779920
Eliaz Bobadilla <71897736+UltiRequiem@users.noreply.github.com>
2021-04-04 15:18:03 -0700

    ✨ Update
    After a long time, some improvements ..
    https://user-images.githubusercontent.com/71897736/113522860-c930a080-9558-11eb-873e-7ca034fb0808.png
    https://stackoverflow.com/questions/15011367/python-module-import-single-line-vs-multi-line

--- spammer.py
+++ spammer.py
@@ -1,31 +1,13 @@
-import pyautogui,time
+import pyautogui
+import time
 
-print("Ready for Spam OwO")
-time.sleep(5)
-f = open("iwannabeadmi.txt",'r')
-for word in f:
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
-  
-'''
-import pyautogui 
-time imports the necessary modules for the python program. Pyautogui is used for automating keyboard and mouse functions, and time is used for letting the program wait before executing the next line(s). 
-
-time.sleep(5) 
-calls the sleep function from the time module and makes the program wait 5 seconds before spamming.
-
-f = open('text.txt', 'r') 
-opens a file (in this example we called it 'text') and puts it in read mode, then it assigns it to a variable (f).
-
-for word in f: 
-says that for each word (or line) there is in the 'f' variable (which contains our file in read mode), it will do certain actions based on what's in the loop.
-
-pyautogui.typewrite(word) 
-calls the typewrite function of the pyautogui module on the variable in the for loop. Basically it automatically types the word or line of your text based on the contents of the text file.
-
-pyautogui.press('enter') 
-calls the keyboard press function of the pyautogui module, and makes the program hit the enter key (as specified in the program).
-
-time.sleep(5)
-will spam someone, so make sure to click on the text box of whatever messaging app you're using to spam before your 5 seconds is up!
-'''
+def run():
+    print("Starting in 5 seconds...")
+    time.sleep(5)
+    f = open("iwannabeadmi.txt", 'r')
+    for word in f:
+        pyautogui.typewrite(word)
+        pyautogui.press("enter")
+    
+if __name__ == '__main__':
+    run()
